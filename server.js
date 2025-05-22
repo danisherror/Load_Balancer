d9a49a6ff188350d6598ceba03c1233d70951ba0
DANISH MAHAJAN <77108977+danisherror@users.noreply.github.com>
2025-05-22 11:34:24 +0530

    adding finding ip and location of the user

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const {lookup} = require('geoip-lite');
 const app = express();
+
 
 const PORT = 3000;
 
@@ -12,9 +14,16 @@ const servers = [
 let current = 0;
 
 app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+const isLocal = ip === '::1' || ip.startsWith('127.') || ip.startsWith('192.168.');
+if (!isLocal) {
+  console.log(ip);
+  console.log(lookup(ip));
+} else {
+  console.log('Local IP detected. Skipping GeoIP lookup.');
+}
   const target = servers[current];
   current = (current + 1) % servers.length;
-
   // Call and return the proxy middleware
   const proxy = createProxyMiddleware({
     target,
